0e3090362cc20b96a8e5a14fe1c0057d85e99554
chaan32 <haechan8864@naver.com>
2025-01-20 19:53:15 +0900

    메인 로직 빼고 테스트 하기

---
+++ tests/test_api/test_no_reset_api_login.py
@@ -0,0 +1,63 @@
+"""
+테스트 해야할 것 
+1. user_sign_up(request : Request, db : Session = Depends(get_db))
+2. user_login(request : Request, db : Session = Depends(get_db))
+
+"""
+
+"""
+def test_create_user(client):
+    test_nickname = "test_nickname"
+    response = client.post(
+        "/users",
+        json={"nickname": test_nickname},
+    )
+    assert response.status_code == 200
+    assert response.json()["nickname"] == test_nickname"""
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+def test_user_sign_up():
+    #given
+    test_login_id = "test"
+    test_login_password = "test"
+    test_user_name = "test_user"
+    test_user_email = "test@example.com"
+
+    #when
+    response = client.post(
+        "/user/sign-up", 
+        json={
+            "login_id" : test_login_id,
+            "login_password" : test_login_password,
+            "name" : test_user_name, 
+            "email" : test_user_email
+        }
+    )
+
+    #then 
+    assert response.status_code == 200
+    assert response.json()["message"] == "성공"
+
+def test_user_login():
+    #given
+    test_login_id = "test"
+    test_login_password = "test"
+
+    #when
+    response = client.post(
+        "/user/login-g", 
+        json={
+            "login_id" : test_login_id,
+            "login_password" : test_login_password
+        }
+    )
+    assert response.status_code == 200
+    assert response.json()["message"] == "성공"
+    assert response.json()["user_login_id"] == "test"
+    assert response.json()["user_id"] == 1
